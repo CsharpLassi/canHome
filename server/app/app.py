a57950578b2ed811aa0235f4771c6e12377511da
Lassi <csharplassi@posteo.de>
2018-04-23 14:24:45 +0200

    Add Basic Server Functions

--- server/app/app.py
+++ server/app/app.py
@@ -1,12 +1,31 @@
+import flask
 from flask import Flask
+
+import serial
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/index')
+def index():
+    return flask.render_template("index.html")
+
+
+@app.route('/on')
+def on():
+    color = flask.request.args.get("color")
+
+    if color is None:
+        return flask.render_template("index.html")
+
+    ser = serial.Serial("/dev/ttyUSB0", 9600)
+    ser.write(("on %s;" % color).encode())
+
+    ser.close()
+
+    return flask.render_template("index.html", color=color)
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=50010)
+    app.run(host="0.0.0.0")
